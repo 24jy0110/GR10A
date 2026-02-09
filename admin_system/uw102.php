10003a23ef464cd9c69ee0d78ccdd763e21b1487
24jy0110 <24jy0110@jec.ac.jp>
2026-02-09 11:42:06 +0900

    利用日数計算方法を修正

--- admin_system/uw102.php
+++ admin_system/uw102.php
@@ -69,7 +69,7 @@ $rideDate = date("Y/m/d H:i", strtotime($res["service_start_time"]));
 
 $start = new DateTime($res["service_start_time"]);
 $end   = new DateTime($res["service_end_date"]);
-$days  = $start->diff($end)->days + 1;
+$days  = $start->diff($end)->days + 2;
 
 /* ドライバー情報 */
 $driverName  = $res["driver_name"] ?: "未定";
