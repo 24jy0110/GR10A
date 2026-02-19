f9b7cffb6e06c58aa600b918bea9dea0f8e21093
24jy0110 <24jy0110@jec.ac.jp>
2026-02-19 13:00:35 +0900

    番号修正

--- admin_system/uw121_01_driver_task_detail.php
+++ admin_system/uw121_01_driver_task_detail.php
@@ -153,7 +153,7 @@ body { font-family:"Noto Sans JP",sans-serif; background:#fafafa; }
 <script>
 function finishTask(no) {
     if (confirm("この行程を完了として登録しますか？")) {
-        location.href = "uw121_02_finish_task.php?r=" + no;
+        location.href = "uw123_01_finish_task.php?r=" + no;
     }
 }
 </script>
